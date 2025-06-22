71ae174bc0b4663aa7f1092a0c7261e32ddd9fdc
timaufdemkampe <tim.aufdemkampe@gmx.de>
2025-06-22 16:32:46 +0200

    Revert "Try to fix header"
    This reverts commit 2e214f45764c71d24f1512e8dc245490a53f3d4d.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,36 +22,24 @@ export const Header: React.FC = () => {
 
   const [mobileNavOpen, setMobileNavOpen] = useState<boolean>(false);
 
-  // Debounce Funktion
-  function debounce<T extends (...args: never[]) => void>(
-    func: T,
-    wait: number
-  ) {
-    let timeout: ReturnType<typeof setTimeout>;
-    return (...args: Parameters<T>) => {
-      clearTimeout(timeout);
-      timeout = setTimeout(() => func(...args), wait);
-    };
-  }
-
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  const memorizedHandleScroll = React.useCallback(
-    debounce(() => {
-      // Schwelle für das Einblenden von StickyNav (z.B. 50px)
-      const scrollY = window.scrollY || window.pageYOffset;
-      setIsHeaderVisible(scrollY < 200);
-    }, 100),
-    []
-  );
+  const memorizedHandleScroll = React.useCallback(() => {
+    const navElement = document.querySelector('.header-container .absolute');
+    if (navElement) {
+      const navRect = navElement.getBoundingClientRect();
+      setIsHeaderVisible(navRect.top > 0);
+    }
+  }, []);
 
   useEffect(() => {
     const handleScroll = memorizedHandleScroll;
+
     window.addEventListener('scroll', handleScroll);
     handleScroll();
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, [memorizedHandleScroll]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
