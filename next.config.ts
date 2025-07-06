77f8abb07a0e64e86df5086c78cf43c32b91c085
TimAufdemKampe <tim.aufdemkampe@gmx.de>
2025-07-06 14:05:46 +0200

    Bug fixes

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  publicRuntimeConfig: {
+    NEXT_PUBLIC_INTERNAL_ACCESS_TOKEN:
+      process.env.NEXT_PUBLIC_INTERNAL_ACCESS_TOKEN,
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+  },
+};
 
 export default nextConfig;
